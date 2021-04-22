ffa951562d39411edd846f9559ad625ecfe7d652
tindrew <tindrewmar@gmail.com>
2021-04-22 03:31:02 +0000

    fixed typo in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   Rails application.routes.draw do
-    root 'applicatio#hello'
+    root 'application#hello'
   end
 end
